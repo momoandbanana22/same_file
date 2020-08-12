3732ed1239e66f6ec6832a0cfd2808c412199dab
momoandbanana <momo_and_banana@yahoo.co.jp>
2020-08-12 21:13:57 +0900

    search double loop

--- same_file.rb
+++ same_file.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
-require 'digest/md5'
+require 'find'
+require 'fileutils'
 
-pp Digest::MD5.file('sample/file/data.dat').to_s
+same_files_pair = []
+added_files = []
+
+target_files = Find.find('D:\Download\video')
+
+target_files.each do |src|
+  target_files.each do |cmp|
+    next unless File.file?(cmp) # ファイル以外は除外
+    next unless File.file?(src) # ファイル以外は除外
+    next if src == cmp # 同じファイル名は除外
+    next if added_files.include?(src) # すでに検査済ファイルは除外
+    next if added_files.include?(cmp) # すでに検査済ファイルは除外
+    next unless FileUtils.cmp(src, cmp) # 内容が違うファイルは除外
+
+    # ファイル名が異なり、内容が同じファイルを記憶
+    same_files_pair.push([src, cmp])
+    added_files.push(src)
+    added_files.push(cmp)
+  end
+end
+
+pp same_files
