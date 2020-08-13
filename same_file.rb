932e3f1acaede8d2b67c699e33a66e56cc38d816
momoandbanana <momo_and_banana@yahoo.co.jp>
2020-08-13 16:32:21 +0900

    zero file fix. #1

--- same_file.rb
+++ same_file.rb
@@ -13,9 +13,17 @@ target_files.each do |src|
     next unless File.file?(cmp) # ファイル以外は除外
     next unless File.file?(src) # ファイル以外は除外
     next if src == cmp # 同じファイル名は除外
-    next if added_files.include?(src) # すでに検査済ファイルは除外
     next if added_files.include?(cmp) # すでに検査済ファイルは除外
-    next unless FileUtils.cmp(src, cmp) # 内容が違うファイルは除外
+
+    if File.zero?(cmp)
+      # ファイルが空なら、ファイルを記憶
+      same_files_pair.push([cmp, ''])
+      added_files.push(cmp)
+      next # 次のファイルへ
+    end
+
+    next if added_files.include?(src) # すでに検査済ファイルは除外
+    next unless FileUtils.cmp(src, cmp) # 内容が異なるファイルは除外
 
     # ファイル名が異なり、内容が同じファイルを記憶
     same_files_pair.push([src, cmp])
